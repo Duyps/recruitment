765c59d528b66a44ab273aca686d0e0961fd1b42
Duy <quocduyps1@gmail.com>
2025-10-08 21:20:31 +0700

    crete file

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import CandidateLogin from "./pages/candidate/CandidateLogin";
+import CandidateRegister from "./pages/candidate/CandidateRegister";
+import CandidateHome from "./pages/candidate/CandidateHome";
+import CompanyLogin from "./pages/company/CompanyLogin";
+import CompanyRegister from "./pages/company/CompanyRegister";
+import CompanyHome from "./pages/company/CompanyHome";
+import NotFound from "./pages/NotFound";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        {/* Candidate */}
+        <Route path="/candidate/login" element={<CandidateLogin />} />
+        <Route path="/candidate/register" element={<CandidateRegister />} />
+        <Route path="/candidate/home" element={<CandidateHome />} />
+        {/* Company */}
+        <Route path="/company/login" element={<CompanyLogin />} />
+        <Route path="/company/register" element={<CompanyRegister />} />
+        <Route path="/company/home" element={<CompanyHome />} />
+        {/* Default */}
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
+  );
 }
-
-export default App
---
+++ src/firebase.js
@@ -0,0 +1,25 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCzIj2s0Sg1ULl4TgPkHWgwrCQj7y59KEI",
+  authDomain: "recruitment-bb526.firebaseapp.com",
+  projectId: "recruitment-bb526",
+  storageBucket: "recruitment-bb526.firebasestorage.app",
+  messagingSenderId: "445394884989",
+  appId: "1:445394884989:web:1b8dbf4122d5e2827e1414",
+  measurementId: "G-L2BHLXBRXE"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+const analytics = getAnalytics(app);
