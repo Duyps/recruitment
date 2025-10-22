6184c5a75a51c0965bf905a99c0142c35f55d80d
Duy <quocduyps1@gmail.com>
2025-10-22 22:20:42 +0700

    đổi hash

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import CandidateHome from "./pages/candidate/CandidateHome";
 import CandidateLogin from "./pages/candidate/CandidateLogin";
@@ -25,32 +25,30 @@ export default function App() {
     <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} />
-        <Route path="/homepage" element={<HomePage/>} />
+        <Route path="/homepage" element={<HomePage />} />
         {/* Candidate */}
-         <Route path="/candidate/home" element={<CandidateLayout />}>
-          <Route index element={<CandidateHome />} /> {/* /candidate/home */}
-          <Route path="saved-jobs" element={<SavedJobsPage />} /> {/* /candidate/home/saved-jobs */}
-          <Route path="account" element={<CandidateAccount />} /> {/* /candidate/home/saved-jobs */}
-          <Route path="review" element={<CompanyReview/>} /> {/* /candidate/home/saved-jobs */}
+        <Route path="/candidate/home" element={<CandidateLayout />}>
+          <Route index element={<CandidateHome />} />
+          <Route path="saved-jobs" element={<SavedJobsPage />} />
+          <Route path="account" element={<CandidateAccount />} />
+          <Route path="review" element={<CompanyReview />} />
           <Route path="review/:companyId" element={<CompanyPublicInfo />} />
-
         </Route>
         <Route path="/candidate/login" element={<CandidateLogin />} />
         <Route path="/candidate/register" element={<CandidateRegister />} />
         <Route path="/candidate/setup" element={<CandidateSetup />} />
-       
+
         {/* Company */}
         <Route path="/company/home" element={<CompanyHome />}>
-          <Route path="create" element={<CreateJob />} /> 
-          <Route path="jobs" element={<ManageJob />} /> 
-          <Route path="profile" element={<CompanyProfile/>} />
+          <Route path="create" element={<CreateJob />} />
+          <Route path="jobs" element={<ManageJob />} />
+          <Route path="profile" element={<CompanyProfile />} />
           <Route path="jobs/:jobId" element={<JobDetail />} />
         </Route>
 
         <Route path="/company/login" element={<CompanyLogin />} />
         <Route path="/company/register" element={<CompanyRegister />} />
         <Route path="/company/setup" element={<CompanySetup />} />
-        
 
         {/* Default */}
         <Route path="/get-started" element={<GetStarted />} />
